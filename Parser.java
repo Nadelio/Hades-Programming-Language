4a22de1295857d9890347e50594fd9e6d3c710f5
Nadelio <carefulsniffle@gmail.com>
2024-11-06 17:19:42 -0600

    Parser finished

--- Parser.java
+++ Parser.java
@@ -40,6 +40,10 @@ public class Parser {
         }
     }
 
+    /**
+    Parses the AST
+    @return ASTC 
+    */
     public ASTC parse(){
         java.util.ArrayList<Command> commands = new java.util.ArrayList<Command>();
         while(this.tok.getType() != Token.TokenType.EOF){
@@ -50,9 +54,14 @@ public class Parser {
         return new ASTC(commands.toArray(new Command[commands.size()]));
     }
 
+    /**
+    Generates the next command from the AST 
+    @return Command
+     */
     private Command nextCommand(){
         Token[] field1;
         Token[] field2;
+        // build command
         switch(this.tok.getType()){
             case Token.TokenType.NOP:
                 return new Command(Token.TokenType.NOP);
@@ -79,185 +88,199 @@ public class Parser {
             case Token.TokenType.POP:
                 return new Command(Token.TokenType.POP);
             case Token.TokenType.MOVE:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.NUMBER){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.MOVE, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.MOVE, field1);
             case Token.TokenType.SET:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.NUMBER){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.SET, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.SET, field1);
             case Token.TokenType.WRITE:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.NUMBER){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.WRITE, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
-                case Token.TokenType.CREATELABEL:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.ALIAS){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.CREATELABEL, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
-                case Token.TokenType.DELETELABEL:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.ALIAS){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.DELETELABEL, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
-                case Token.TokenType.JUMPLABEL:
-                field1 = new Token[3];
-                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                    this.readToken();
-                    field1[0] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.ALIAS){
-                        this.readToken();
-                        field1[1] = tok;
-                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.JUMPLABEL, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.WRITE, field1);
+            case Token.TokenType.CREATELABEL:
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.CREATELABEL, field1);
+            case Token.TokenType.DELETELABEL:
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.DELETELABEL, field1);
+            case Token.TokenType.JUMPLABEL:
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.JUMPLABEL, field1);
             case Token.TokenType.CALLDEPENDENCY:
-            field1 = new Token[3];
-            if(this.peekToken().getType() == Token.TokenType.LBRACKET){
-                this.readToken();
-                field1[0] = tok;
-                if(this.peekToken().getType() == Token.TokenType.ALIAS){
-                    this.readToken();
-                    field1[1] = tok;
-                    if(this.peekToken().getType() == Token.TokenType.RBRACKET){
-                            this.readToken();
-                            field1[2] = tok;
-                            return new UnaryCommand(Token.TokenType.CALLDEPENDENCY, field1);
-                        } else {
-                            exitWithError();
-                        }
-                    } else {
-                        exitWithError();
-                    }
-                } else {
-                    exitWithError();
-                }
-                break;
+                field1 = this.doAliasField();
+                return new UnaryCommand(Token.TokenType.CALLDEPENDENCY, field1);
             case Token.TokenType.SYSCALL:
                 field1 = new Token[7];
-                // TODO: syscall
-                // ? 5 labels OR numbers
+                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
+                    this.readToken();
+                    field1[0] = tok;
+                    if(this.peekToken().getType() == Token.TokenType.ALIAS || this.peekToken().getType() == Token.TokenType.NUMBER){
+                        this.readToken();
+                        field1[1] = tok;
+                        if(this.peekToken().getType() == Token.TokenType.ALIAS || this.peekToken().getType() == Token.TokenType.NUMBER){
+                            this.readToken();
+                            field1[2] = tok;
+                            if(this.peekToken().getType() == Token.TokenType.ALIAS || this.peekToken().getType() == Token.TokenType.NUMBER){
+                                this.readToken();
+                                field1[3] = tok;
+                                if(this.peekToken().getType() == Token.TokenType.ALIAS || this.peekToken().getType() == Token.TokenType.NUMBER){
+                                    this.readToken();
+                                    field1[4] = tok;
+                                    if(this.peekToken().getType() == Token.TokenType.ALIAS || this.peekToken().getType() == Token.TokenType.NUMBER){
+                                        this.readToken();
+                                        field1[5] = tok;
+                                        if(this.peekToken().getType() == Token.TokenType.RBRACKET){
+                                            this.readToken();
+                                            field1[6] = tok;
+                                            return new UnaryCommand(Token.TokenType.SYSCALL, field1);
+                                        } else {
+                                            exitWithError(this.peekToken().getType());
+                                        }
+                                    } else {
+                                        exitWithError(this.peekToken().getType());
+                                    }
+                                } else {
+                                    exitWithError(this.peekToken().getType());
+                                }
+                            } else {
+                                exitWithError(this.peekToken().getType());
+                            }
+                        } else {
+                            exitWithError(this.peekToken().getType());
+                        }
+                    } else {
+                        exitWithError(this.peekToken().getType());
+                    }
+                } else {
+                    exitWithError(this.peekToken().getType());
+                }
                 break;
             case Token.TokenType.COMMENT:
                 ArrayList<Token> content = new ArrayList<Token>();
-                // TODO: comment
-                // ? add to content until sees comment token again
-                break;
+                while(this.peekToken().getType() != Token.TokenType.COMMENT){
+                    this.readToken();
+                    content.add(this.tok);
+                }
+                return new UnaryCommand(Token.TokenType.COMMENT, content.toArray(new Token[content.size()]));
             case Token.TokenType.CREATEDEPENDENCY:
-                //TODO: create dependency
-                break;
+                field1 = new Token[5];
+                field2 = new Token[3];
+
+                // build field1
+                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
+                    this.readToken();
+                    field1[0] = tok;
+                    if(this.peekToken().getType() == Token.TokenType.ALIAS){
+                        this.readToken();
+                        field1[1] = tok;
+                        if(this.peekToken().getType() == Token.TokenType.FILEINDENTIFIER){
+                            this.readToken();
+                            field1[2] = tok;
+                            if(this.peekToken().getType() == Token.TokenType.EXTENSION){
+                                this.readToken();
+                                field1[3] = tok;
+                                if(this.peekToken().getType() == Token.TokenType.RBRACKET){
+                                    this.readToken();
+                                    field1[4] = tok;
+                                } else {
+                                    exitWithError(this.peekToken().getType());
+                                }
+                            } else {
+                                exitWithError(this.peekToken().getType());
+                            }
+                        } else {
+                            exitWithError(this.peekToken().getType());
+                        }
+                    } else {
+                        exitWithError(this.peekToken().getType());
+                    }
+                } else {
+                    exitWithError(this.peekToken().getType());
+                }
+
+                // build field2
+                field2 = this.doAliasField();
+
+                return new BinaryCommand(Token.TokenType.CREATEDEPENDENCY, field1, field2);
             case Token.TokenType.INTERRUPT:
-                //TODO: interrupt
-                break;
+                field1 = new Token[5];
+                field2 = new Token[3];
+
+                // build field1
+                if(this.peekToken().getType() == Token.TokenType.LBRACKET){
+                    this.readToken();
+                    field1[0] = tok;
+                    if(this.peekToken().getType() == Token.TokenType.ALIAS){
+                        this.readToken();
+                        field1[1] = tok;
+                        if(this.isComparison(this.peekToken().getType())){
+                            this.readToken();
+                            field1[2] = tok;
+                            if(this.peekToken().getType() == Token.TokenType.ALIAS){
+                                this.readToken();
+                                field1[3] = tok;
+                                if(this.peekToken().getType() == Token.TokenType.RBRACKET){
+                                    this.readToken();
+                                    field1[4] = tok;
+                                } else {
+                                    exitWithError(this.peekToken().getType());
+                                }
+                            } else {
+                                exitWithError(this.peekToken().getType());
+                            }
+                        } else {
+                            exitWithError(this.peekToken().getType());
+                        }
+                    } else {
+                        exitWithError(this.peekToken().getType());
+                    }
+                } else {
+                    exitWithError(this.peekToken().getType());
+                }
+
+                // build field2
+                field2 = this.doAliasField();
+
+                return new BinaryCommand(Token.TokenType.INTERRUPT, field1, field2);
             case Token.TokenType.LOOP:
-                //TODO: loop
+                ArrayList<Command> loopBody = new ArrayList<Command>();
+                if(this.peekToken().getType() == Token.TokenType.LBRACKET){ this.readToken(); } else { exitWithError(this.peekToken().getType()); }
+                while(this.peekToken().getType() != Token.TokenType.RBRACKET){ loopBody.add(this.nextCommand()); }
+                if(this.peekToken().getType() == Token.TokenType.RBRACKET){ this.readToken();
+                    return new LoopCommand(loopBody.toArray(new Command[loopBody.size()]));
+                } else { exitWithError(this.peekToken().getType()); }
                 break;
+            default: break;
         }
         return new Command(Token.TokenType.INVALID);
     }
 
-    private void exitWithError(){
-        System.out.println("Syntax error: expected LBRACKET @ " + this.position);
+    private void exitWithError(Token.TokenType type){
+        System.out.println("Syntax error: expected " + type + " @ " + this.position);
         System.out.println("Recieved: " + this.peekToken().getType());
         System.exit(1);
+    }
+
+    private boolean isComparison(Token.TokenType type){ return type == Token.TokenType.EQUAL || type == Token.TokenType.NOTEQUAL || type == Token.TokenType.GREATER || type == Token.TokenType.LESS || type == Token.TokenType.GREATEREQUAL || type == Token.TokenType.LESSEQUAL; }
+    private Token[] doAliasField(){
+        Token[] field = new Token[3];
+        if(this.peekToken().getType() == Token.TokenType.LBRACKET){
+            this.readToken();
+            field[0] = tok;
+            if(this.peekToken().getType() == Token.TokenType.ALIAS){
+                this.readToken();
+                field[1] = tok;
+                if(this.peekToken().getType() == Token.TokenType.RBRACKET){
+                    this.readToken();
+                    field[2] = tok;
+                } else {
+                    exitWithError(this.peekToken().getType());
+                }
+            } else {
+                exitWithError(this.peekToken().getType());
+            }
+        } else {
+            exitWithError(this.peekToken().getType());
+        }
+        return field;
     }
 }
